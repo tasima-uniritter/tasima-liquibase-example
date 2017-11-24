7aa9abd3544bfc71398ff51fc9987acead839ad4
Alex S. Rosa <alexsros@gmail.com>
2017-11-24 21:32:05 -0200

    first commit

---
+++ src/main/java/br/edu/tasimaliquibaseexample/TasimaLiquibaseExampleApplication.java
@@ -0,0 +1,12 @@
+package br.edu.tasimaliquibaseexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasimaLiquibaseExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasimaLiquibaseExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/br/edu/tasimaliquibaseexample/TasimaLiquibaseExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.tasimaliquibaseexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TasimaLiquibaseExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
